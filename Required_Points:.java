46e3b559157ff25e5afe48269cea97a2b2b12989
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2024-07-31 15:38:49 +0530

    import java.util.Scanner; public class RequiredPoints{     public static void main(String[] args){         Scanner read =  new Scanner(System.in);         int x,y;         x = read.nextInt();         y = read.nextInt();         System.out.println(y/x);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class RequiredPoints{
+    public static void main(String[] args){
+        Scanner read =  new Scanner(System.in);
+        int x,y;
+        x = read.nextInt();
+        y = read.nextInt();
+        System.out.println(y/x);
+    }
+}
